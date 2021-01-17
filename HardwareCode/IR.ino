f2ac2d4b63dfbec1e97ecd0805931ec290bb37e1
JasonWu2020 <48867921+JasonWu2020@users.noreply.github.com>
2021-01-17 00:52:40 -0800

    Update IR.ino

--- HardwareCode/IR.ino
+++ HardwareCode/IR.ino
@@ -1,11 +1,19 @@
 void setup() {
     pinMode(A2, INPUT);
-    pinMode(2, OUTPUT);
+    //pinMode(2, OUTPUT);
+    pinMode(3, OUTPUT);
     Serial.begin(9600);
 }
 
 void loop() {
-    Serial.println(analogRead(A2));
+    int IRval = analogRead(A2);
+    Serial.println(IRval);
+    while (analogRead(A2) <= 615) {
+        tone(3, 880, 500);
+        delay(500);
+        tone(3, 440, 500);
+        delay(500);
+    }
     delay(100);
 
 }
